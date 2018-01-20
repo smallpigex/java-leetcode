acc6be146652623a85e24660d18e5f4db83da716
hwchu <smallpigex@gmail.com>
2018-01-20 18:30:54 +0800

    add test case

--- src/test/java/easy/JudgeRouteCircleTest.java
+++ src/test/java/easy/JudgeRouteCircleTest.java
@@ -1,14 +1,30 @@
 package easy;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class JudgeRouteCircleTest {
-
+  JudgeRouteCircle jrc;
+  
+  @Before
+  public void setUp() throws Exception {
+    jrc = new JudgeRouteCircle();
+  }
+  
   @Test
-  public void test() {
-    JudgeRouteCircle jrc = new JudgeRouteCircle();
+  public void testOriginalPlace() {
     assertTrue(jrc.judgeCircle("UD"));
+    assertTrue(jrc.judgeCircle("LR"));
+    assertTrue(jrc.judgeCircle("URLD"));
+    assertTrue(jrc.judgeCircle("ULRD"));
+  }
+  
+  @Test
+  public void testNotOriginalPlace() {
+    assertFalse(jrc.judgeCircle("UDD"));
+    assertFalse(jrc.judgeCircle("ULL"));
+    assertFalse(jrc.judgeCircle("ULDRR"));
   }
 
 }
